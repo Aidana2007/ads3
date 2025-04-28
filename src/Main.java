b34d70eb44786aaf7ad9eec463f60e334ee92cb2
Aidana2007 <maksutova2007@inbox.ru>
2025-04-28 14:57:53 +0500

    testing

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,14 @@ public class Main {
             }
             System.out.println("Bucket " + i + ": " + count + " elements");
         }
+        BST<Integer, String> tree = new BST<>();
+        tree.put(5, "Five");
+        tree.put(3, "Three");
+        tree.put(7, "Seven");
+        tree.put(1, "One");
+
+        for (var elem : tree) {
+            System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
+        }
     }
 }
