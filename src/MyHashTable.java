efd5bc7518b92db62ed24af2f412906b80587bd4
Aidana2007 <maksutova2007@inbox.ru>
2025-04-28 14:02:32 +0500

    implement HashNode

---
+++ src/MyHashTable.java
@@ -0,0 +1,20 @@
+public class MyHashTable<K, V> {
+
+    // Inner class for HashNode
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+}
+
