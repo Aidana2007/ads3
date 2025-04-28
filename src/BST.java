cbb3a0b592c5cb1c143bd57f643ba6ba677f48d6
Aidana2007 <maksutova2007@inbox.ru>
2025-04-28 14:54:39 +0500

    add methods in BST

--- src/BST.java
+++ src/BST.java
@@ -3,6 +3,7 @@ import java.util.Stack;
 
 public class BST<K extends Comparable<K>, V> implements Iterable<BST.Entry<K, V>> {
     private Node root;
+    private int size = 0;
 
     private class Node {
         private K key;
@@ -31,9 +32,133 @@ public class BST<K extends Comparable<K>, V> implements Iterable<BST.Entry<K, V>
         }
     }
 
-    // methods to implement later
-    public void put(K key, V val) {}
-    public V get(K key) { return null; }
-    public void delete(K key) {}
-    public Iterator<Entry<K, V>> iterator() { return null; }
+    public void put(K key, V val) {
+        Node newNode = new Node(key, val);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        Node current = root;
+        Node parent = null;
+        while (current != null) {
+            parent = current;
+            if (key.compareTo(current.key) < 0) {
+                current = current.left;
+            } else if (key.compareTo(current.key) > 0) {
+                current = current.right;
+            } else {
+                current.val = val;
+                return;
+            }
+        }
+
+        if (key.compareTo(parent.key) < 0) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+        }
+        size++;
+    }
+    public V get(K key) {
+        Node current = root;
+        while (current != null) {
+            if (key.compareTo(current.key) < 0) {
+                current = current.left;
+            } else if (key.compareTo(current.key) > 0) {
+                current = current.right;
+            } else {
+                return current.val;
+            }
+        }
+        return null;
+    }
+    public void delete(K key) {
+        Node parent = null;
+        Node current = root;
+
+        while (current != null && !current.key.equals(key)) {
+            parent = current;
+            if (key.compareTo(current.key) < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+
+        if (current == null) {
+            return; // Not found
+        }
+
+        // Case 1: Node with only one child or no child
+        if (current.left == null || current.right == null) {
+            Node newCurr;
+            if (current.left == null) {
+                newCurr = current.right;
+            } else {
+                newCurr = current.left;
+            }
+
+            if (parent == null) {
+                root = newCurr;
+            } else if (current == parent.left) {
+                parent.left = newCurr;
+            } else {
+                parent.right = newCurr;
+            }
+        } else {
+            // Case 2: Node with two children
+            Node p = null;
+            Node temp;
+
+            temp = current.right;
+            while (temp.left != null) {
+                p = temp;
+                temp = temp.left;
+            }
+
+            if (p != null) {
+                p.left = temp.right;
+            } else {
+                current.right = temp.right;
+            }
+
+            current.key = temp.key;
+            current.val = temp.val;
+        }
+        size--;
+    }
+    public int size() {
+        return size;
+    }
+
+    public Iterator<Entry<K, V>> iterator() {
+        return new Iterator<>() {
+            private Stack<Node> stack = new Stack<>();
+            private Node current = root;
+
+            {
+                while (current != null) {
+                    stack.push(current);
+                    current = current.left;
+                }
+            }
+
+            public boolean hasNext() {
+                return !stack.isEmpty();
+            }
+
+            public Entry<K, V> next() {
+                Node node = stack.pop();
+                Entry<K, V> entry = new Entry<>(node.key, node.val);
+                Node right = node.right;
+                while (right != null) {
+                    stack.push(right);
+                    right = right.left;
+                }
+                return entry;
+            }
+        };
+    }
+
 }
